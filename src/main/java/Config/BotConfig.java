973bbe4bf5988bf518c0adbdfbd7782606e6c309
Krishna Kandula <krishna.c.kandula@gmail.com>
2016-11-06 11:47:34 -0600

    Removed BotConfig from cache

--- src/main/java/Config/BotConfig.java
+++
@@ -1,9 +0,0 @@
-package Config;
-
-/**
- * Created by krishnakandula on 11/6/16.
- */
-public abstract class BotConfig {
-    public static final String BOT_USERNAME = "WTP_bot";
-    public static final String BOT_TOKEN = "<token>";
-}
